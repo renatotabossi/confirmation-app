d5671e4d80ad3ed81851a8b064e9bc69f8599a78
Renato Tabossi <r.tabossi.inf@gmail.com>
2023-01-10 09:45:21 -0300

    makes nav-bar, drawer and main navigation, some changes.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { Route, BrowserRouter as Router, Routes} from "react-router-dom";
 
 import CreateAccount from "./account/create/create-account"
-import ChangePassword from './account/change/forgot_password';
+import ChangePassword from './account/change/forgot-password';
 import SignIn from "./account/login/login";
+import MainNavigation from './navigation/main-navigation';
 
 /*
  * tsrcc -> create template class
  * tsrsfc -> create template function
+ * rfc -> best function template
  */
 
 function App() {
@@ -18,6 +20,7 @@ function App() {
       <Route path="/" element={<SignIn />}/>
       <Route path="/create" element={<CreateAccount />}/>
       <Route path="/forgot" element={<ChangePassword />}/>
+      <Route path="/navigation" element={<MainNavigation />}/>
     </Routes>
     </Router>
   );
---
+++ client/src/navigation/main-navigation.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SearchAppBar from './nav-bar/nav-bar'
+
+export default function MainNavigation() {
+  return (
+     <>
+      <SearchAppBar/>
+      <h1>hello</h1>
+     </>
+  )
+}
